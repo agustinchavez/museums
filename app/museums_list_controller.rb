01c840ecef57eab083d66c01b524d3c60687b613
agustinchavez <chavez13@illinois.edu>
2016-08-06 14:59:46 -0500

    added list view, empty

--- app/museums_list_controller.rb
+++ app/museums_list_controller.rb
@@ -1,5 +1,17 @@
 class MuseumsListController < UITableViewController
+  CELLID = 'CellId'
   def viewWillAppear(animated)
     navigationController.setNavigationBarHidden(false, animated:true)
+  end
+
+  def tableView(tableView, numberOfRowsInSection:section)
+    Museum.all.count
+  end
+
+
+  def tableView(tableView, cellForRowAtIndexPath:indexPath)
+    cell = UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:CELLID)
+    cell.accessoryType = UITableViewCellAccessoryDetailDisclosureButton
+    cell
   end
 end
